0d849579c5160faed18ea34fea210a7572845c66
Artur Iskhakov <artur.iskhakov.dev@outlook.com>
2025-12-11 20:16:57 +0500

    feat: add environment configuration file and update .gitignore to exclude .env files

---
+++ src/shared/config/env-config.ts
@@ -0,0 +1,8 @@
+export const siteName = process.env.NEXT_PUBLIC_SITE_NAME || 'Site Name';
+
+export const title = process.env.NEXT_PUBLIC_TITLE || 'Your Website Title';
+
+export const description =
+  process.env.NEXT_PUBLIC_DESCRIPTION || 'Your website description goes here.';
+
+export const baseURL = process.env.NEXT_PUBLIC_URL || 'https://your-domain.com';
