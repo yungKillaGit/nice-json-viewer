4e3d3dc24d22c2c898d32eebea9daccc76a7af03
Artur Iskhakov <artur.iskhakov.dev@outlook.com>
2025-12-11 21:05:51 +0500

    sync package lock

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from 'next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import {
   baseURL,
